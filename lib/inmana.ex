2a61c89179e6ad1169189673f068b89bfe0d75b2
Raul Pereira <raul.pereira@gmail.com>
2021-05-08 19:21:12 -0300

    class#3: add create supply route

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,5 +1,9 @@
 defmodule Inmana do
   alias Inmana.Restaurants.Create, as: CreateRestaurant
 
+  alias Inmana.Supplies.Create, as: CreateSupply
+
   defdelegate create_restaurant(params), to: CreateRestaurant, as: :call
+
+  defdelegate create_supply(params), to: CreateSupply, as: :call
 end
